90f54993027539493e13a5fc886241bcb0fc5831
Сергей Цымбалов <106691442+Serjio99@users.noreply.github.com>
2026-04-08 13:23:39 +0300

    Remove dead auth entity mapping

--- src/main/java/com/musicplatform/auth/repository/AuthRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.musicplatform.auth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-import com.musicplatform.auth.entity.AuthEntity;
-
-public interface AuthRepository extends JpaRepository<AuthEntity, UUID> {
-}
